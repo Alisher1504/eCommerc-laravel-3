63968f1d88d834f87e6a1e11792976e75ff1f6f5
Alisher1504 <ruzmetov150499@gmail.com>
2023-02-08 14:37:03 +0500

    all order_details table create successfully

--- database/migrations/2023_02_08_090549_create_order_detailes_table.php
+++ database/migrations/2023_02_08_090549_create_order_detailes_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('order_detailes', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('address1', 255);
+            $table->string('address2', 255);
+            $table->string('city', 255);
+            $table->string('state', 45)->nullable();
+            $table->string('zip_code', 45);
+            $table->string('country_code', 3);
             $table->timestamps();
         });
     }
